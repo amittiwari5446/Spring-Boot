b528cb5c60cb819a93b065c23f9a8acde8b332f6
amittiwari5446 <99649731+amittiwari5446@users.noreply.github.com>
2024-10-05 11:49:00 +0530

    Add files via upload

---
+++ ActuatorsDemo/src/test/java/com/example/actuatorsdemo/ActuatorsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actuatorsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
